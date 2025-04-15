f638be4eb4787400c7e85f2c1feded56d155469f
Shubham Kumar <shubham@glassfrog.design>
2025-04-15 13:42:35 +0530

    new changes

--- components/layout/PageHead.js
+++ components/layout/PageHead.js
@@ -5,7 +5,7 @@ const PageHead = ({ headTitle }) => {
         <>
             <Head>
                 <title>
-                    {headTitle ? headTitle : "Eduvalt - Online Courses & Education Nextjs Template"}
+                    {headTitle ? headTitle : "Vtrain "}
                 </title>
             </Head>
         </>
